bd4fde2f51893a9873f5a7fd8034b8e6c3123d4b
Xavier's Coding Club <fxcodingclub@gmail.com>
2020-09-25 15:25:07 -0400

    First Commit

---
+++ Main.py
@@ -0,0 +1,48 @@
+##
+#
+# Author: FX Coding Club
+# Date 9/24/2020
+#
+
+
+# These are the possible choices in the game, using the randint function, the computer will be a choice based on the integer it chooses
+list = ['rock', 'paper', 'scissors']
+
+
+
+
+
+
+# Compare var1 and computerChoice to see who wins
+
+# The following is all the comparisons if the computer chooses "Rock"
+if computerChoice == 'rock' and  var1 == 'scissors'):
+    print('You chose ' + var1 + ' and the computer chose ' +
+          computerChoice + ', The computer wins!')
+
+elif computerChoice == 'rock' and  var1 == 'paper'):
+    print('You chose ' + var1 + ' and the computer chose ' +
+          computerChoice + ', You win!')
+
+# The following is all the comparisons if the computer chooses "Paper"
+elif computerChoice == 'paper' and  var1 == 'rock'):
+    print('You chose ' + var1 + ' and the computer chose ' +
+          computerChoice + ', The computer wins!')
+
+elif computerChoice == 'paper' and  var1 == 'scissors'):
+    print('You chose ' + var1 + ' and the computer chose ' +
+          computerChoice + ', You win!')
+
+# The following is all the comparisons if the computer chooses "Scissors"
+elif computerChoice == 'scissors' and  var1 == 'paper'):
+    print('You chose ' + var1 + ' and the computer chose ' +
+          computerChoice + ', The computer wins!')
+
+elif computerChoice == 'scissors' and  var1 == 'rock'):
+    print('You chose ' + var1 + ' and the computer chose ' +
+          computerChoice + ', You win!')
+
+# The case in which both the computer and the user chose the same one, and they tie
+else:
+    print('You chose ' + var1 + ' and the computer chose ' +
+          computerChoice + ', you tied!')
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Orange-Red-Github
- 
+---
+## Rock, Paper, Scissors
+
+This repo is meant to test your skill in forking/cloning and editing another person's repo. This is code for a simple rock paper scissors game, we ask you to make the following edits (sorted from esiest to hardest):
+
+- Add a variable "computerChoice" and make it be randomly assigned a valid choice
+- Change variables to be more readable (i.e turn "var1" to "Choice")
+- Implement lizard Spock fucntionality: ![How R.P.S.L.S works](imgs/rpsls.jpg)
+- Add input validation 
+
