8f23161f5b32af035383776841ca7b12b515a6fd
VishGit1234 <>
2020-11-02 09:15:33 -0500

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,44 +1,83 @@
 ##
 #
-# Author: FX Coding Club
-# Date 9/24/2020
+# Author: VishGit1234
+# Date 11/2/2020
 #
-
+import random
+from random import randint
 
 # These are the possible choices in the game, using the randint function, the computer will be a choice based on the integer it chooses
-list = ['rock', 'paper', 'scissors']
+list = ['rock', 'paper', 'scissors', 'spock', 'lizard']
 
+# Assign a random valid choice to computerChoice (computer answer)
+computerChoice = list[randint(0, 2)]
 
-# Compare var1 and computerChoice to see who wins
+# Get user input and store
+isValid = False
+while isValid == False:
+    print('Input one of the following; rock, paper, scissors, spock or lizard')
+    Choice = input()
+    Choice = Choice.lower()
+    for i in list:
+        if Choice == i:
+            isValid = True
+    if isValid == False:
+        print(Choice + ' is an incorrect input')
+
+# Compare Choice and computerChoice to see who wins
 
 # The following is all the comparisons if the computer chooses "Rock"
-if computerChoice == 'rock' and var1 == 'scissors'):
-    print('You chose ' + var1 + ' and the computer chose ' +
+if computerChoice == 'rock' and Choice == 'scissors':
+    print('You chose ' + Choice + ' and the computer chose ' +
           computerChoice + ', The computer wins!')
 
-elif computerChoice == 'rock' and var1 == 'paper'):
-    print('You chose ' + var1 + ' and the computer chose ' +
+elif computerChoice == 'rock' and Choice == 'paper':
+    print('You chose ' + Choice + ' and the computer chose ' +
           computerChoice + ', You win!')
 
+elif computerChoice == 'rock' and Choice == 'spock':
+    print('You chose ' + Choice + ' and the computer chose ' +
+          computerChoice + ', You win!')
+
+elif computerChoice == 'rock' and Choice == 'lizard':
+    print('You chose ' + Choice + ' and the computer chose ' +
+          computerChoice + ', The computer wins!')
+    
 # The following is all the comparisons if the computer chooses "Paper"
-elif computerChoice == 'paper' and var1 == 'rock'):
-    print('You chose ' + var1 + ' and the computer chose ' +
+elif computerChoice == 'paper' and Choice == 'rock':
+    print('You chose ' + Choice + ' and the computer chose ' +
           computerChoice + ', The computer wins!')
 
-elif computerChoice == 'paper' and var1 == 'scissors'):
-    print('You chose ' + var1 + ' and the computer chose ' +
+elif computerChoice == 'paper' and Choice == 'scissors':
+    print('You chose ' + Choice + ' and the computer chose ' +
+          computerChoice + ', You win!')
+
+elif computerChoice == 'paper' and Choice == 'spock':
+    print('You chose ' + Choice + ' and the computer chose ' +
+          computerChoice + ', The computer wins!')
+
+elif computerChoice == 'paper' and Choice == 'lizard':
+    print('You chose ' + Choice + ' and the computer chose ' +
           computerChoice + ', You win!')
 
 # The following is all the comparisons if the computer chooses "Scissors"
-elif computerChoice == 'scissors' and var1 == 'paper'):
-    print('You chose ' + var1 + ' and the computer chose ' +
+elif computerChoice == 'scissors' and Choice == 'paper':
+    print('You chose ' + Choice + ' and the computer chose ' +
           computerChoice + ', The computer wins!')
 
-elif computerChoice == 'scissors' and var1 == 'rock'):
-    print('You chose ' + var1 + ' and the computer chose ' +
+elif computerChoice == 'scissors' and Choice == 'rock':
+    print('You chose ' + Choice + ' and the computer chose ' +
           computerChoice + ', You win!')
+
+elif computerChoice == 'scissors' and Choice == 'spock':
+    print('You chose ' + Choice + ' and the computer chose ' +
+          computerChoice + ', You win!')
+
+elif computerChoice == 'scissors' and Choice == 'lizard':
+    print('You chose ' + Choice + ' and the computer chose ' +
+          computerChoice + ', The computer wins!')
 
 # The case in which both the computer and the user chose the same one, and they tie
 else:
-    print('You chose ' + var1 + ' and the computer chose ' +
+    print('You chose ' + Choice + ' and the computer chose ' +
           computerChoice + ', you tied!')
